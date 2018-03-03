b3a7213ee9f067ca187d97ffae6d94eb49a9dc3b
Kingsley <asromafanisme@gmail.com>
2018-03-04 00:41:17 +0800

    sample code to share image via whatsapp

--- app/src/main/java/com/example/mkhoi/sharedhouse/MainActivity.kt
+++ app/src/main/java/com/example/mkhoi/sharedhouse/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.mkhoi.sharedhouse
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore.Images
 import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import com.example.mkhoi.sharedhouse.fees_view.FeesFragment
 import com.example.mkhoi.sharedhouse.monthly_bill.MonthlyBillFragment
-import com.example.mkhoi.sharedhouse.room_edit.EditRoomFragment
 import com.example.mkhoi.sharedhouse.rooms_view.RoomsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -32,6 +41,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         supportFragmentManager.beginTransaction()
                 .replace(R.id.main_content_fragment, MonthlyBillFragment.newInstance())
                 .commitNow()
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    0)
+        }
+
     }
 
     override fun onBackPressed() {
@@ -63,9 +80,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .addToBackStack(FeesFragment::class.java.canonicalName)
                         .commit()
             }
+            R.id.nav_send -> {
+                sendWhatsAppMsg()
+            }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun sendWhatsAppMsg() {
+        val b = Bitmap.createBitmap(nav_view.getWidth(), nav_view.getHeight(), Bitmap.Config.ARGB_8888)
+        val c = Canvas(b)
+        nav_view.draw(c)
+
+        val bytes = ByteArrayOutputStream()
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = Images.Media.insertImage(contentResolver, b, "Title", null)
+        val imageUri =  Uri.parse(path)
+        val sendIntent = Intent(Intent.ACTION_SEND)
+        sendIntent.`package` = "com.whatsapp"
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "My sample image text");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        sendIntent.setType("image/jpeg");
+        sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        sendIntent.putExtra("jid", "6594572814@s.whatsapp.net")
+        startActivity(sendIntent)
     }
 }
